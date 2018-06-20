7f454d6580094dba1b68abedb90b15a45593cf44
TechMaster980 <league@WTS-IM-6.attlocal.net>
2018-06-20 11:57:24 -0700

    3 days here

---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,12 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "you are so awesome and cool at programming");
+		System.out.println("this is awesome");
+		JOptionPane.showInputDialog("what is your name");
+	}
+
+}
---
+++ src/section3/greeter.java
@@ -0,0 +1,11 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class greeter {
+	public static void main(String[] args) {
+		
+		String name =  JOptionPane.showInputDialog("what is your name");
+		JOptionPane.showMessageDialog(null, "hi " + name);
+	}
+}
